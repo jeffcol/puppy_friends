eb30a059f45a179b0190cfeecee5a4bb46edb9f2
jeffcole <villa8ks@gmail.com>
2020-08-23 11:32:54 -0500

    Updates

---
+++ precache-manifest.c9d51f8acfaff8d080e2ff483edad31a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa711185a9b2fea17eb1ca089552d84a",
+    "url": "/jeffcol/puppy_friends/index.html"
+  },
+  {
+    "revision": "316ac755af432deeb3d8",
+    "url": "/jeffcol/puppy_friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c1a0476559a39ad23fdb",
+    "url": "/jeffcol/puppy_friends/static/css/main.2ae47733.chunk.css"
+  },
+  {
+    "revision": "316ac755af432deeb3d8",
+    "url": "/jeffcol/puppy_friends/static/js/2.de095742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jeffcol/puppy_friends/static/js/2.de095742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a0476559a39ad23fdb",
+    "url": "/jeffcol/puppy_friends/static/js/main.42c6196c.chunk.js"
+  },
+  {
+    "revision": "3f29f59c0d1f58d796df",
+    "url": "/jeffcol/puppy_friends/static/js/runtime-main.0ef0cedd.js"
+  },
+  {
+    "revision": "572bb423c4bff50c41ad3698fe267aec",
+    "url": "/jeffcol/puppy_friends/static/media/GOODDC.572bb423.TTF"
+  }
+]);
